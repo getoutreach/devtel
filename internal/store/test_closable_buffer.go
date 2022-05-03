05830d57a841e79d433bb7885d15673622ec5619
Peter Lisy <peter.lisy@outreach.io>
2022-05-03 10:09:39 -0700

    fix: Taking things OSS (#3)
    * fix: Taking things OSS
    
    * rebootstrapped.
    
    * removed octos
    
    * removed extra license

--- internal/store/test_closable_buffer.go
+++ internal/store/test_closable_buffer.go
@@ -1,11 +1,17 @@
+// Copyright 2022 Outreach Corporation. All Rights Reserved.
+
+// Description: This package contains the implementation of mock io.Closer
+
 package store
 
 import "bytes"
 
+// TestClosableBuffer wraps a bytes.Buffer and implements io.Closer.
 type TestClosableBuffer struct {
 	bytes.Buffer
 }
 
+// Close implements io.Closer. Does nothing.
 func (TestClosableBuffer) Close() error {
 	return nil
 }
